50a2afdee643f961c4c8f812f82f7e89b876982b
Alyssa Pratt <alyssakpratt@gmail.com>
2021-06-05 14:37:15 -0500

    adds component styling

--- ge-challenge/src/components/longitude.js
+++ ge-challenge/src/components/longitude.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import {Card} from 'react-bootstrap';
-
+import {Card, Form, Button, Container, Row, Col} from 'react-bootstrap';
 const LongCard = (props) => {
     return (
-        <Card style={{width: '18rem'}}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-            </Card.Body>
-        </Card>
+        <Container fluid>
+            <Row>
+            <Col>
+            <Card style={{width: '25rem', margin: '20rem auto auto auto',}}>
+                
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>longitude</Form.Label>
+                        <Form.Control type="text"/>
+                        <Form.Text className="text-muted">
+                        (example : -4.4203400)
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Next
+                    </Button>
+                </Form>
+
+            </Card>
+            </Col>
+            </Row>
+        </Container>
     );
 };
 
